79a8cbb13e974fec1e4ca16838110655997cde70
Daiana C <dauipoma@gmail.com>
2024-03-16 20:00:11 +0100

    packages exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ module com.example.javafx {
 
     opens com.example.javafx to javafx.fxml;
     exports com.example.javafx;
+    exports com.example.javafx.controller;
+    opens com.example.javafx.controller to javafx.fxml;
+    exports com.example.javafx.model;
+    opens com.example.javafx.model to javafx.fxml;
 }
